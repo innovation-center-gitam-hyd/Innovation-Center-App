0a5ac024ada1283a9a5ea50ab3113438795b428f
Rajdeep Ray <rayrajdeep2001@gmail.com>
2020-04-13 02:32:42 +0530

    commit to 'master'

--- lib/myScreen/inventory.dart
+++ lib/myScreen/inventory.dart
@@ -11,15 +11,41 @@ class _MyInventoryState extends State<MyInventory> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "Innovation Center | Inventory",
+          "Innovation Center",
+          style: TextStyle(fontFamily: 'TimesNewRoman', fontSize: 25),
         ),
         backgroundColor: Colors.black,
         leading: IconButton(
           icon: Icon(Icons.arrow_back_ios),
           onPressed: () => Navigator.pop(context),
         ),
+        centerTitle: true,
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          Container(
+            color: Colors.black,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Center(
+                    child: Text(
+                      "Inventory List",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
