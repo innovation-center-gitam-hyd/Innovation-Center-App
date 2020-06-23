8ebe07dcbb3cf9efda5187dccae8b75534eba003
Rajdeep Ray <rayrajdeep2001@gmail.com>
2020-06-23 21:40:46 +0530

    commit

--- lib/myScreen/Project/project.dart
+++ lib/myScreen/Project/project.dart
@@ -10,7 +10,7 @@ class _MyProjectState extends State<MyProject> {
   @override
   Widget build(BuildContext context) {
     return MyScaffold(
-      title: "Create Project",
+      title: "Create new Project",
       myBody: Container(),
     );
   }
